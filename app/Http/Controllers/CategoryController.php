ec746d03510cc9b94ef69eef40e52417663636e6
sujalAwal <awalsujal99@gmail.com>
2024-09-06 01:24:04 +0545

    Ajax implemts in category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,42 +5,62 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends Controller
 {
     public function index(){
         $data = Category::select('id','name','status')->get();
-        return view('category.index' ,compact('data'));
+        return response()->json($data,200);
     }
 
     public function create(){
         return view('category.create');
     }
 
-    public function store(Request $request){
-        try{
+    public function store(Request $request){ 
+       
+          
+        if(empty($request->postId)){
             $request->validate([
-            'name'=> 'required|min:3',
-            'status'=>'required|between:0,1'
-        ]);
+                'name'=> 'required|min:3',
+                'status'=>'required|between:0,1'
+            ]);
 
-         Category::create([
-            'name'=>$request->name,
-            'status'=>$request->status,
+            Category::create([
+                'name'=>$request->name,
+                'status'=>$request->status,
+    
+            ]);
+    
+            return response()->json(['success'=>"Category successfully Added"],200);
+        }else{
+            $request->validate([
+                'name' => 'required|min:3|unique:categories,name,' . ($request->postId ?? ''),
+                'status'=>'required|between:0,1'
+            ]);
+    
+            $category = Category::findOrFail($request->postId);
+    
+             $category->update([
+                'name'=>$request->name,
+                'status'=>$request->status,
+    
+            ]);
+    
+            return response()->json(['success'=>"Category successfully updated"],200);
+        }
+       
+        }
 
-        ]);
-
-        return redirect()->route('category.index')->with('success',"Category successfully Added");
-    }
-    catch(Exception $e){
-        return redirect()->back()->with('error',$e->getMessage());
         
-    }
-    }
+    
+   
 
     public function edit(int $id){
         $category = Category::findOrFail($id);
-        return view('category.edit',compact('category'));
+        // return view('category.edit',compact('category'));
+        return response()->json($category,200);
     }
 
     public function update(Request $req , int $id){
@@ -72,22 +92,17 @@ class CategoryController extends Controller
 
             $item =Category::findOrFail($id);     
             $item->delete();
-            return redirect()->back()->with('success' ,"Category Has Deleted");
+            return response()->json(['success'=>"Category Has Deleted"],200);
         }catch(Exception $exception){
-            return redirect()->back()->with('error',$exception->getMessage());
+            return response()->json(['error'=>$exception->getMessage()],500);
         }
     }
 
     public function changeStatus(int $id){
         $item = Category::findOrFail($id);
-        $sts = $item->status;
-        if($sts == 1){
-            $item->status = "0";
-        }else{
-            $item->status = "1";
-
-        }
+       
+        $item->status = $item->status == "1" ? "0" : "1";
         $item->save();
-        return redirect()->back();
+        return response()->json(['message'=>"successfully updated"],200);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                       <li class="nav-item">
-                        <a class="nav-link  font-monospace btn btn-outline-warning "  href="{{route('category.index')}}">Category</a>
+                        <a class="nav-link  font-monospace btn btn-outline-warning " id="catNav">Category</a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link font-monospace p-2 btn btn-outline-warning ml-3" href="{{route('post.index')}}">Post</a>
@@ -85,14 +85,287 @@
             
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 mainDiv">
             @yield('content')
         </main>
+<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color:#22ea08; color:white; border-radius:5px">
+        <h1 class="modal-title fs-5"  id="modelLabel"></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body"  style="background-color: rgb(230, 208, 13); color:rgb(0, 92, 252);font-size:20px;  ">
+        <form method="POST" class="modalForm">
+          @csrf
+          <input type="text" hidden id="unqId" name="postId" >
+          <div class="mb-3">
+            <label for="postName" class="form-label">Name</label>
+            <input type="text" name="name" class="form-control" id="postName" aria-describedby="name">
+            
+          <div class="mb-3">
+            <label for="postStauts" class="form-label">Status</label>
+            <select class="form-select" id="postStauts" name="status" aria-label="Default select status">
+              <option  value="" hidden></option>
+              <option  value="1">Active</option>
+              <option value="0">In-Active</option>
+            </select>
+          </div>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn sendBtn"></button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
   @yield('script')
-</script>
+  function load(){
+          $.ajax({
+        url:" {{route('category.index')}} ",
+        dataType:'json',
+        success:function(success){
+      let tableRow = success.map((data,i)=>{
+          let statusButton = data.status == 1 ? `<button type="submit" class="statusBtn" style="background-color: rgb(50, 239, 17); border-radius: 18px; padding: 3px; border: 2px solid rgb(50,239,17)" id="${data.id}">
+             <span>Active</span>
+           </button>`
+        : `<button type="submit" class="statusBtn" style="background-color: rgb(233, 28, 17); border-radius: 18px; padding: 3px; border: 2px solid rgb(233,28,17)" id="${data.id}">
+             <span>Inactive</span>
+           </button>`;
+            return `  <tr>
+                            <th scope="row">${i+1}</th>
+                            <td>${data.name}</td>
+                            <td>
+                                
+                              ${statusButton}
+                           
+                            </td>
+                            <td>  <div class="row">
+                                <div class="col">
+                                    <!-- Edit Button -->
+                                    <button data-bs-toggle="modal" data-bs-target="#modal" id="edit" data-bs-id="${data.id}"  href=" route('category.edit') " class="btn btn-primary">
+                                        <i class="fas fa-edit"></i> Edit
+                                    </button>
+                    
+                                    <!-- Delete Button (with confirmation dialog) -->
+                                    
+                                        <button type="button" class="btn btn-danger deleteCategory" id="${data.id}" >
+                                            <i class="fas fa-trash">Delete</i> 
+                                        </button>
+                                        
+                                    
+                                </div>
+                            </div>
+                        </td>
+                          </tr>`}).join('')
+                         $(".mainDiv").html(` <div class="container">
+        <div class="row justify-content-center m-5">
+            <div class="col-md-8">
+                <div class="card" style="background-color: rgb(230, 208, 13); color: rgb(0, 92, 252); font-size: 20px;">
+                    <div class="position-relative card-header text-success text-center text-monospace" style="height: 100px; font-family: Georgia, 'Times New Roman', Times, serif; font-size: 2rem">
+                        {{ __('Category Section') }}
+                        <button data-bs-toggle="modal" id="addNew" data-bs-target="#modal" data-bs-whatever="@getbootstrap"  class="bg-success bg-opacity-70 border border-info border-start-0 rounded-start position-absolute bottom-0 end-0">
+                            <h5 class="text-white" style="text-decoration: none; font-size: 1rem">ADD NEW</h5>
+                        </button>
+                    </div>
+                    <div class="card-body text-center">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">S.N.</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                ${tableRow}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`);
+          
+  
+        
+        }
+      }
+    );
+  }
+  $(function(){
+    $("#catNav").click(function(){
+     load();
+
+    })
+   
+   
+    //status Changing
+    $(document).on('click', '.statusBtn', function(e) {
+            e.preventDefault(); // Prevent default button action
+           let id= $(this).attr('id');
+            $.ajax({
+              url:`{{ route('category.changeStatus', ':id') }}`.replace(':id', id),
+              type:'PATCH',
+              data:{
+                _token:'{{csrf_token()}}'
+              },
+              success:function(response){
+                Swal.fire({
+                  position: "top-end",
+                 icon: "success",
+                title: response.message,
+               showConfirmButton: false,
+                timer: 1000
+               } ).then(() => {
+                load();
+            
+        });
+              },
+              error: function(xhr, status, error) {
+            // Optional: Handle errors
+            console.error('AJAX Error:', status, error);
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'An error occurred',
+                text: error,
+                showConfirmButton: true
+            });
+        }
+            })
+            
+        });
+    
+  })
+ 
+  const modal = document.getElementById('modal');
+
+if (modal) {
+  modal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget;
+    const btnId = button.getAttribute('id');
+    if(btnId === "edit"){
+      
+      $(function () {
+        const id = button.getAttribute('data-bs-id');
+        $(".modal-title").text("Edit Category");
+        $(".sendBtn").text("Update Category");
+        $(".sendBtn").addClass("btn-success");
+
+        $.ajax({
+          url:`{{route('category.edit','cngid')}}`.replace("cngid",id),
+          type:'GET',
+          header:{
+            'X-CSRF-TOKEN':' {{csrf_token()}}'
+
+          },
+          success:function(res){
+       
+            $("#postName").val(res.name);
+            $("#postStauts").val(res.status);          
+            $("#unqId").val(res.id);
+          },
+          error: function(xhr, status, error) {
+            // Optional: Handle errors
+            console.error('AJAX Error:', status, error);
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'An error occurred',
+                text: error,
+                showConfirmButton: true
+            });
+        }
+        })
+      })
+
+    }
+    else if(btnId === "addNew"){
+      $(function () {
+        $("#postName").val('');
+        $("#postStauts").val('');
+        $(".sendBtn").removeClass("btn-success");
+        $(".sendBtn").addClass("btn-primary");
+        $(".modal-title").text("Add New Category");
+        $(".sendBtn").text("Create Category");
+      
+      })
+    }
+    
+  
+  });
+}
+
+$(document).on("submit",".modalForm",function(e){
+
+  e.preventDefault();
+  const formData = $(this).serialize();
+  console.log(formData);
+  
+  $.ajax({
+    url:"{{ route('category.store') }}",
+    method:'POST',
+      data:formData,
+      contentType: 'application/x-www-form-urlencoded',
+      success:(res)=>{
+        Swal.fire({     
+                   icon:"success",   
+                   title: 'Successfull ',  
+                    html: res.success    });
+                   $("#modal").modal('hide');
+                   load();
+      
+      },
+      error: function(xhr) {  
+          let errors = xhr.responseJSON.errors;    
+          let errorMessage = '';  
+            $.each(errors, function(key, value) {        errorMessage += value + '<br>';    });  
+              Swal.fire({     
+                   icon: 'error',   
+                   title: 'Validation Error',  
+                         html: errorMessage    });
+                        }
+      
+    
+        })
+      
+      })
+      $(document).on("click",".deleteCategory",(e)=>{
+      e.preventDefault();
+      let deleteid= e.currentTarget.id;
+     $.ajax({
+      url:`{{route('category.destroy','id')}}`.replace("id",deleteid),
+      type:"DELETE",
+      headers:{
+            'X-CSRF-TOKEN':' {{csrf_token()}}'
+
+          },
+          success:(res)=>{
+            Swal.fire({
+                  position: "top-end",
+                 icon: "success",
+                title: res.success,
+               showConfirmButton: false,
+                timer: 1000
+               } ).then(() => {
+                load();
+            
+        });
+          }
+
+     })
+      
+   
+    });
+        
+      </script>
 </html>
